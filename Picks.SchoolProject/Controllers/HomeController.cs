c47ee7e1c7ef9359f417e39269971fa6aecf94cc
Eric <Hjelpenlindros@gmail.com>
2018-01-29 19:04:26 +0100

    image upload
    *skickar nu korrekt bilden och vald kategori till server side, sparad i en mapp på i servern foldern och sparar sökväg + bild info i SQL databasen.
    
    bugs -
    *sidan laddar inte om efter sparad bild.
    *koppla bilden till en befintlig kategori.

--- Picks.SchoolProject/Controllers/HomeController.cs
+++ Picks.SchoolProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,16 +7,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Picks.SchoolProject.Data;
 using Picks.SchoolProject.Models;
+using System.Web;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace Picks.SchoolProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public HomeController(ApplicationDbContext context)
+        public HomeController(ApplicationDbContext context, IHostingEnvironment environment)
         {
             _context = context;
+            _hostingEnvironment = environment;
         }
 
         public async Task<IActionResult> Index()
@@ -31,21 +38,38 @@ namespace Picks.SchoolProject.Controllers
         {       
             if (ModelState.IsValid)
             {
-                Category Category = new Category();
+                var newCategory = new Category();
                 {
-                    Category.Name = category;
+                    newCategory.Name = category;
                 }
-                _context.Add(Category);
+                _context.Categories.Add(newCategory);
                 await _context.SaveChangesAsync();
             }
 
             return RedirectToAction("Index", "Home");
         }
 
-        [Route("api/create")]
-        public async Task<IActionResult> SaveImage()
+        [Route("api/file-upload/image")]
+        public async Task<IActionResult> SaveImage(IFormFile file, string name)
         {
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "images");
+            var guid = Guid.NewGuid();
+            var filePath = Path.Combine(uploads, guid + file.FileName);
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
 
+            var category = _context.Categories.FirstOrDefault(x => x.Name == name);
+
+            var newImage = new Image();
+            {
+                newImage.Uploaded = DateTime.UtcNow;
+                newImage.Url = filePath;
+            }
+
+            _context.Images.Add(newImage);
+            _context.SaveChanges();
 
             return RedirectToAction("Index", "Home");
         }
