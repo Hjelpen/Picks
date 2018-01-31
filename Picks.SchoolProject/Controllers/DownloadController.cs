918cca3d2b170cee99114ecdc74c2484ecd480c7
Eric <Hjelpenlindros@gmail.com>
2018-01-31 22:33:03 +0100

    work
    *Ladda ner bild funkar
    *Sortera bilder efter vald kategori
    *Små fixes/förbättringar.
    
    bug fixad:
    *ladda om sidan efter ny uppladdad bild.

--- Picks.SchoolProject/Controllers/DownloadController.cs
+++ Picks.SchoolProject/Controllers/DownloadController.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.OData.Atom;
 using Picks.SchoolProject.Data;
+using Picks.SchoolProject.Models;
 
 namespace Picks.SchoolProject.Controllers
 {
@@ -20,9 +23,12 @@ namespace Picks.SchoolProject.Controllers
 
         public IActionResult Download()
         {
-            var images = _context.Images.ToList();
+ 
+            var allImages = _context.Images.ToList();
 
-            return View(images);
+            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
+
+            return View(allImages);
         }
 
         public IActionResult DownloadImage (string url)
@@ -30,6 +36,22 @@ namespace Picks.SchoolProject.Controllers
             var filepath = @"~/images/" + url;
 
             return File(filepath, System.Net.Mime.MediaTypeNames.Application.Octet, Path.GetFileName(filepath));
+        }
+
+        public IActionResult Filter(int id)
+        {
+            if (id == 0)
+            {
+                ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
+                var allImages = _context.Images.ToList();
+                return View("Download", allImages);
+            }
+
+            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
+
+            var images = _context.Images.Where(x => x.CategoryId == id).ToList();
+
+            return View("Download", images);
         }
     }
 }
