ca4933d1b0ba229c891fc97f2b162cef99db30ef
Eric <Hjelpenlindros@gmail.com>
2018-02-07 20:38:50 +0100

    cacheExentesion

---
+++ Picks.SchoolProject/Utility/CacheExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Picks.SchoolProject.Utility
+{
+    public static class CacheExtensions
+    {
+        public static void SetValue<T>(this IDistributedCache cache, string key, T value)
+        {
+            cache.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T GetValue<T>(this IDistributedCache cache, string key)
+        {
+            var value = cache.GetString(key);
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+
+    }
+}
